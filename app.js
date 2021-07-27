8d74d1480a18e88f7285b087d07b8de657e4951c
Miguel <migueltriaca@gmail.com>
2021-07-28 00:50:29 +0800

    changes to port variable

--- app.js
+++ app.js
@@ -116,6 +116,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(5000, () => {
-  console.log('listening on port 5000');
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
